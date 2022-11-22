a1bc6f4de3a2eec7e900feee944b360f1343fb35
Hary Pirajan Mahalingam <hary2309@gmail.com>
2022-11-22 08:49:24 +0000

    Improve user model unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,12 +11,22 @@ describe 'User' do
   end
 
   context 'when I create a Google-user' do
+    it 'can be found by find_google_user' do
+      original_user = create(:user, :google_user, uid: 'SOME-RANDOM-GOOGLE-UID')
+      expect(User.find_google_user('SOME-RANDOM-GOOGLE-UID')).to eq original_user
+    end
+
     it 'has correct auth provicer' do
       expect(create(:user, :google_user).auth_provider).to eq 'Google'
     end
   end
 
   context 'when I create a Github-user' do
+    it 'can be found by find_github_user' do
+      original_user = create(:user, :github_user, uid: 'SOME-RANDOM-GITHUB-UID')
+      expect(User.find_github_user('SOME-RANDOM-GITHUB-UID')).to eq original_user
+    end
+
     it 'has correct auth provicer' do
       expect(create(:user, :github_user).auth_provider).to eq 'Github'
     end

0d6e293c6fd6ec0d4ed2b324303f8828ff1d2ed2
Gordon Choi <gchoi5738@berkeley.edu>
2022-11-22 22:58:32 +0000

    fix new lint issues

--- features/step_definitions/representative_steps.rb
+++ features/step_definitions/representative_steps.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
 Given /the representatives exist/ do |representatives_table|
   representatives_table.hashes.each do |representative|
-    Representative.create!(name: representative[:name], ocdid: representative[:divisionId], title: representative[:office])
+    Representative.create!(name: representative[:name], ocdid: representative[:divisionId], 
+                           title: representative[:office])
   end
 end
 
@@ -10,7 +12,6 @@ Then /^I should see the representatives: (.*)$/ do |representative_list|
   end
 end
 
-When /^I navigate to the (.* County) in (.*)$/ do |county, state|
-  state = State.find_by(name: state)
+When /^I navigate to the (.* County) in (.*)$/ do |_county, _state|
   visit search_representatives_path(address: 'San Francisco County CA')
 end
